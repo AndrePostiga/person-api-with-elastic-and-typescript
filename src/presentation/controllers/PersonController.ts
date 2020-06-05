063bac10872999338897b70d07f4a68bc43437db
Andre Postiga <andre.postiga@msn.com>
2020-06-04 21:44:58 -0300

    feat: ensure ProductController returns 400 if an invalid parameter is provided

--- src/presentation/controllers/PersonController.ts
+++ src/presentation/controllers/PersonController.ts
@@ -5,8 +5,17 @@
 import { badRequest } from '../helpers/HttpHelper';
 import { MissingParamError } from '../errors/MissingParameter';
 import { HttpRequest, HttpResponse } from '../interfaces/http';
+import { Controller } from '../interfaces/controller';
+import { Validator } from '../interfaces/validator';
+import { InvalidParamError } from '../errors/InvalidParameter';
 
-export class PersonController {
+export class PersonController implements Controller {
+  private readonly validator: Validator;
+
+  constructor(validator: Validator) {
+    this.validator = validator;
+  }
+
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'last_name', 'color', 'gender', 'birth_date', 'profession'];
 
@@ -16,9 +25,14 @@ export class PersonController {
       }
     }
 
+    const isValid = this.validator.isValid(httpRequest.body.color);
+    if (!isValid) {
+      return badRequest(new InvalidParamError('color'));
+    }
+
     return {
-      statusCode: 400,
-      body: new MissingParamError('something'),
+      statusCode: 200,
+      body: true,
     };
   }
 }
